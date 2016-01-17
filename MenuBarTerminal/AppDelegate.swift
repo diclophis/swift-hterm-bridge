0468d8c2fe38d1e5aad4ca76de31e07e13c0a5ea
Jon Bardin <diclophis@gmail.com>
2016-01-16 20:27:55 -0800

    first working web view in menu bar functional

--- MenuBarTerminal/AppDelegate.swift
+++ MenuBarTerminal/AppDelegate.swift
@@ -7,21 +7,39 @@
 //
 
 import Cocoa
+import WebKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
 
+    
+    @IBOutlet weak var terminaltem: NSMenuItem!
+    @IBOutlet weak var terminalView: TerminalView!
+    
+    @IBOutlet weak var webView: WebView!
+    
+    @IBAction func quitClicked(sender: AnyObject) {
+        
+        NSApplication.sharedApplication().terminate(self)
+        
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        
+        let icon = NSImage(named: "statusIcon")
+        //icon?.template = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+        terminaltem.view = terminalView
+        webView.mainFrame.loadRequest(NSURLRequest(URL: NSURL(string: "https://archive.org")!))
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
---
+++ MenuBarTerminal/TerminalView.swift
@@ -0,0 +1,22 @@
+//
+//  TerminalView.swift
+//  MenuBarTerminal
+//
+//  Created by mavenlink on 1/16/16.
+//
+//
+
+import Cocoa
+import WebKit
+
+class TerminalView: NSView {
+
+    @IBOutlet weak var webView: WebView!
+    
+//    override func drawRect(dirtyRect: NSRect) {
+//        super.drawRect(dirtyRect)
+//
+//        // Drawing code here.
+//    }
+    
+}
